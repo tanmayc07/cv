12b202ff7b706be9aaefb1bc9913520fc8365373
Tanmay Chaudhari <hctanmay@gmail.com>
2024-01-15 16:59:09 -0500

    Update resume data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,147 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Tanmay Chaudhari",
+  initials: "TC",
+  location: "Binghamton, New York",
+  locationLink: "https://www.google.com/maps/place/Binghamton",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Computer Programmer",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "A Master's in Computer Science student at Binghamton University making result-driven and user-focused applications.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/48885301?v=4",
+  personalWebsiteUrl: "https://tanmayc.me/personal-site",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "hctanmay@gmail.com",
+    tel: "+16072968139",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/tanmayc07",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/hctanmay/",
         icon: LinkedInIcon,
       },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
-      },
+      // {
+      //   name: "X",
+      //   url: "https://x.com/[name]",
+      //   icon: XIcon,
+      // },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "State University of New York at Binghamton",
+      degree: "Master's of Science in Computer Science - 3.8 GPA",
+      start: "2023",
+      end: "2025",
+    },
+    {
+      school: "University of Mumbai",
+      degree: "Bachelor's Degree in Computer Engineering - 8.7 CGPA",
+      start: "2018",
+      end: "2022",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "Dezignolics pvt. ltd",
+      // link: "https://parabol.co",
       badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      title: "Machine Learning Intern",
+      // logo: ParabolLogo,
+      start: "Feb 2021",
+      end: "Mar 2021",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+        "Trained extensively on AI/ML Concepts in Python, leveraging libraries such as Scikit-learn, Matplotlib, Seaborn, and Pandas. Created and fine-tuned machine learning models for a house price prediction problem, achieving a 15% increase in predictive accuracy compared to baseline models. Processed and analyzed a dataset of over 100K samples, deriving insights leading to a 35% improvement in model performance through feature engineering and selection.",
     },
     {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
+      company: "Trivia Softwares",
+      // link: "https://clevertech.biz",
       badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      title: "Java Developer Intern",
+      // logo: ClevertechLogo,
+      start: "June 2019",
+      end: "August 2019",
+      description:
+        "Led contributions to various projects involving Java applications with GUI and Database Connectivity, demonstrating proficiency in Java development. Spearheaded development of a comprehensive Student Management System Software Project using Java Swing Library, ensuring",
     },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "C",
+    "Java",
+    "Python",
+    "Flask",
+    "Haskell/Golang",
+    "Scikit-Learn",
+    "Nodejs/Reactjs",
+    "Docker/NGINX/Git-Github",
+    "Linux/Windows/MacOS",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Write.it",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Team Project",
+        "Nodejs",
+        "Expressjs",
+        "MongoDB",
+        "Ejs",
+        "JWT"
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "A Full-Stack Web Blogging Application",
+      // logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "writeit",
+        href: "https://github.com/tanmayc07/Blog-App",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "FIFA Player Stats Analysis",
+      techStack: ["Team Project", "Python", "Flask", "MongoDB Atlas"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "Analysis of FIFA Player Stats for last 10 years using non-trivial NoSQL queries.",
+      // logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "fifastats",
+        href: "https://github.com/tanmayc07/CS532_Nosql_Project",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
+      title: "tanmayc.me",
+      techStack: ["Side Project", "Hugo", "PaperMod", "CSS"],
       description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+        "My personal website and blog. Powered by Hugo and PaperMod with custom CSS.",
+      // logo: JarockiMeLogo,
       link: {
         label: "github.com",
-        href: "https://jarocki.me/",
+        href: "https://tanmayc.me/personal-site",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Vaccine Efficiency Predictor",
+      techStack: ["Team Project", "Python", "Scikit-learn", "Streamlit"],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
+        "Automating Prediciton of B-cell Epitope for Vaccine Efficiency Estimation.",
+      // logo: Minimal,
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "vaccine",
+        href: "https://github.com/tanmayc07/WWCodeHackathon",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Weather Dashboard",
+      techStack: ["Side Project", "Python", "Django", "OpenWeatherMap API"],
       description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+        "Simple and functional Weather Dashboard Web App.",
+      // logo: Minimal,
       link: {
         label: "github.com",
-        href: "https://parabol.co/",
+        href: "https://github.com/tanmayc07/WeatherDashboard",
       },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
