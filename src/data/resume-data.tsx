f8f7ecef2f8fdfb8fae3e7502021b4b3fd709630
Tanmay Chaudhari <hctanmay@gmail.com>
2024-07-07 11:48:19 -0400

    Patch work ex

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -67,13 +67,22 @@ export const RESUME_DATA = {
   ],
   work: [
     {
+      company: "Ugam - A Merkle Company",
+      badges: [],
+      title: "Associate Analyst",
+      start: "June 2022",
+      end: "May 2023",
+      description: 
+        "Designed and deployed over 50 survey forms using HTML, CSS, JavaScript, and Python, leveraging Dynata for efficient and accurate data collection, resulting in a 30% increase in response rates and significantly enhancing the overall data quality. Implemented and automated end-to-end data pipelines with SQL, Python, and Apache Airflow, significantly reducing data processing time by 40% and improving data accuracy by 25%, while also enhancing the overall data handling efficiency. Constructed interactive dashboards using Tableau and Power BI, enhancing decision-making and efficiency by 20%, and automated internal tools with Python and Shell scripting to optimize project management, saving 100+ hours monthly"
+    },
+    {
       company: "Dezignolics pvt. ltd",
       // link: "https://parabol.co",
       badges: [],
       title: "Machine Learning Intern",
       // logo: ParabolLogo,
-      start: "Feb 2021",
-      end: "Mar 2021",
+      start: "January 2021",
+      end: "June 2021",
       description:
         "Trained extensively and acquired expertise in AI/ML Concepts predominantly with Python programming language, leveraging libraries such as Scikit-learn, Matplotlib, Seaborn, and Pandas. Created and fine-tuned machine learning models for a house price prediction problem, achieving a 15% increase in predictive accuracy compared to baseline models. Processed and analyzed a dataset of over 100K samples, deriving insights leading to a 35% improvement in model performance through feature engineering and selection.",
     },
@@ -84,18 +93,20 @@ export const RESUME_DATA = {
       title: "Java Developer Intern",
       // logo: ClevertechLogo,
       start: "June 2019",
-      end: "August 2019",
+      end: "December 2019",
       description:
         "Led and contributed to diverse Java application projects, skillfully incorporating Graphical User Interfaces (GUI) and Database Connectivity, demonstrating a high level of proficiency in Java development. Spearheaded development of a Student Management System Software Project utilizing Java Swing Library, ensuring a user-friendly interface at frontend and achieved a 20% improvement in efficiency and user satisfaction through intuitive design and functional implementation.",
     },
   ],
   skills: [
     "C",
+    "C++",
     "Java",
     "Python",
     "Flask",
-    "Haskell/Golang",
+    "SQL",
     "Scikit-Learn",
+    "Tensorflow",
     "Nodejs/Reactjs",
     "Docker/NGINX/Git-Github",
     "Linux/Windows/MacOS",
